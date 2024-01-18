0f61cb79ec3278f2823665c536851aba09613bec
G-Audige <gloriaaudige@gmail.com>
2024-01-18 11:01:18 -0500

    Initial layout made

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import axios from 'axios';
+// Components
+import Content from './components/Content/Content';
+import Navbar from './components/Navbar/Navbar';
+// Hooks
+import { useState, useEffect } from 'react';
 
+const apiKey = process.env.Petfinder_API_Key;
 function App() {
+  const [search, setSearch] = useState(null);
+  // const getSearch = async () => {
+  //   const url = 'https://omgvamp-hearthstone-v1.p.rapidapi.com/cardbacks';
+  //   const options = {
+  //     method: 'GET',
+  //     headers: {
+  //       'X-RapidAPI-Key': 'f40703f20dmsh86ac0d16e9c5d33p17af6bjsn62dcd48ef5a9',
+  //       'X-RapidAPI-Host': 'omgvamp-hearthstone-v1.p.rapidapi.com',
+  //     },
+  //   };
+
+  //   try {
+  //     const response = await fetch(url, options);
+  //     const result = await response.text();
+  //     console.log(result);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // };
+  // useEffect(() => {
+  //   getSearch();
+  // }, [search]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Navbar />
+      <Content />
     </div>
   );
 }
---
+++ src/pages/IngredientSearchPage/IngredientSearchPage.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function IngredientSearchPage() {
+  return <div>IngredientSearchPage</div>;
+}
